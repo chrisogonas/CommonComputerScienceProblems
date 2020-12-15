f13726284ce89ed069cfeaeb400c5abdc8eb64ea
Chrisogonas O. Mc'Odhiambo <chrisogonas@yahoo.com>
2020-12-15 05:53:18 -0500

    More linked list operations

---
+++ GetLinkNodeDataFromTail.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CommonComputerScienceProblems
+{
+    class GetLinkNodeDataFromTail
+    {
+        static int getNode(Node head, int positionFromTail)
+        {
+            Node tempNode = head;
+            int len = 0;
+            int data = 0;
+
+            while (tempNode != null)
+            {
+                tempNode = tempNode.next;
+                if (tempNode != null) { len++; }
+            }
+
+            int posn = len - positionFromTail;
+            tempNode = head; // re-initialize head
+            while (tempNode != null && posn >= 0)
+            {
+                if (posn == 0)
+                {
+                    data = tempNode.Data;
+                    break;
+                }
+                posn--;
+                tempNode = tempNode.next;
+            }
+
+            return data;
+        }
+    }
+}
---
+++ RemoveLinkedListDuplicates.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CommonComputerScienceProblems
+{
+    class RemoveLinkedListDuplicates
+    {
+        static Node removeDuplicates(Node head)
+        {
+            Node current = head;
+            Node newHead = null;
+            bool headSet = false;
+            while (current != null)
+            {
+                if (!headSet)
+                {
+                    newHead = current;
+                    headSet = true;
+                }
+                if (current.next != null)
+                {
+                    if (current.next.Data == current.Data)
+                    {
+                        if (current.next.next != null)
+                        {
+                            current.next = current.next.next; // skip similar node
+                        }
+                        else
+                        {
+                            current.next = current.next.next; // skip last similar node
+                            break; // end of list
+                        }
+                    }
+                    else
+                    { // neighbors are unique
+                        current = current.next;
+                    }
+                }
+                else
+                {
+                    current = current.next; // advance to null if at the end
+                }
+            }
+
+            return newHead;
+        }
+    }
+}
